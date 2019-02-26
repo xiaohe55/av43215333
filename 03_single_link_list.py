018abef65451f16ebcc1de60574799c3dbc18c89
coyote <1185462886@qq.com>
2019-02-26 08:27:53 +0800

    0226

--- 03_single_link_list.py
+++ 03_single_link_list.py
@@ -7,7 +7,7 @@
 '''
 
 
-class Node():
+class Node(object):
     '''节点'''
 
     def __init__(self, elem):
@@ -15,20 +15,20 @@ class Node():
         self.next = None
 
 
-class SingleLinkList():
+class SingleLinkList(object):
     """单链表"""
 
     def __init__(self, node=None):
-        self._head = node
+        self.__head = node
 
     def is_empty(self):
         """链表是否为空"""
-        return self._head == None
+        return self.__head == None
 
     def length(self):
         """链表长度"""
         # cur游标，用来移动遍历节点
-        cur = self._head
+        cur = self.__head
         # count 记录数据
         count = 0
         while cur != None:
@@ -38,37 +38,81 @@ class SingleLinkList():
 
     def travel(self):
         """遍历整个链表"""
-        cur = self._head
+        cur = self.__head
         while cur != None:
-            print(cur.elem)
+            print(cur.elem, end=" ")
             cur = cur.next
+        print("")
 
     def add(self, item):
-        """链表头部添加元素"""
-        pass
+        """链表头部添加元素，头插法"""
+        node = Node(item)
+        node.next = self.__head
+        self.__head = node
 
     def append(self, item):
         """链表尾部添加元素"""
         node = Node(item)
         if self.is_empty():
-            self._head = node
+            self.__head = node
         else:
-            cur = self._head
+            cur = self.__head
             while cur.next != None:
                 cur = cur.next
             cur.next = node
 
     def insert(self, pos, item):
-        """指定位置添加元素"""
-        pass
+        """
+        指定位置添加元素
+        :param pos: 从0开始
+        :param item:
+        :return:
+        """
+        if pos < 0:
+            self.add(item)
+        elif pos > (self.length() - 1):
+            self.append(item)
+        else:
+            pre = self.__head
+            count = 0
+            while count < (pos - 1):
+                count += 1
+                pre = pre.next
+            # 当循环退出后，pre指向pos-1的位置
+            node = Node(item)
+            node.next = pre.next
+            pre.next = node
 
     def remove(self, item):
-        """删除节点"""
-        pass
+        '''
+        删除节点
+        :param item:
+        :return:
+        '''
+        cur = self.__head
+        pre = None
+        while cur != None:
+            if cur.elem == item:
+                # 先判断此节点是否为头节点
+                # 头节点
+                if cur == self.__head:
+                    self.__head = cur.next
+                else:
+                    pre.next = cur.next
+                break
+            else:
+                pre = cur
+                cur = cur.next
 
     def search(self, item):
         """查找节点是否存在"""
-        pass
+        cur = self.__head
+        while cur != None:
+            if cur.elem == item:
+                return True
+            else:
+                cur = cur.next
+        return False
 
 
 if __name__ == '__main__':
@@ -83,6 +127,20 @@ if __name__ == '__main__':
     ll.append(2)
     ll.append(3)
     ll.append(4)
+    ll.add(8)
     ll.append(5)
     ll.append(6)
+    # 8 1 2 3 4 5 6
+    ll.insert(-1, 9)  # 9 8 1 2 3 4 5 6
+    ll.travel()
+
+    ll.insert(3, 100)  # 9 8 1 100 2 3 4 5 6
+    ll.travel()
+
+    ll.insert(10, 200)  # 9 8 1 100 2 3 4 5 6 200
+    ll.travel()
+
+    ll.remove(9)
+    ll.travel()
+    ll.remove(200)
     ll.travel()
